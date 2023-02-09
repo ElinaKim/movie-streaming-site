8caf297bb684803c19fc1a52cfd21ab03793aa95
Elina Kim <elinakim791@gmail.com>
2023-02-08 17:40:43 -0800

    create actor and movie tables

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask 
+from peewee import *
 import peewee
 from dotenv import load_dotenv
 import os
@@ -8,11 +9,29 @@ app = Flask(__name__)
 load_dotenv()
 conn_str = os.getenv("CONN_STR") 
 
-database = peewee.PostgresqlDatabase(conn_str)  
+db = peewee.PostgresqlDatabase(conn_str)  
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Actor(BaseModel):
+    name = CharField(50)
+
+
+class Movie(BaseModel):
+    actor_id = ForeignKeyField(Actor, backref = 'actor_id')
+    title = CharField(50)
+    year = IntegerField
+    time = IntegerField
+
 
 @app.route('/')
 def index():
     return 'Hello World.'
+
+db.connect()
+db.create_tables([Actor, Movie])
 
 if __name__ == '__main__':
     app.run()
